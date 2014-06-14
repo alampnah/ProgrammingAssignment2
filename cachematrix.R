b9bbc1deb3995a7beccc1d42cdd75301aa288b4c
Alampnah <alampnah@yahoo.com>
2014-06-14 01:38:15 -0400

    Full Implementation V1
    Comments need to be added

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
+## 
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverseOfX <- NULL
+    set <- function(y) {
+        x <<- y
+        inverseOfX <<- NULL
+    }
+    get <- function() x
+    setCacheSolve <- function(invofx) inverseOfX <<- invofx
+    getCacheSolve <- function() inverseOfX
+    list(set = set, get = get,
+         setCacheSolve = setCacheSolve,
+         getCacheSolve = getCacheSolve)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inverseOfX <- x$getCacheSolve()
+    if(!is.null(inverseOfX)) {
+        message("getting cached data")
+        return(inverseOfX)
+    }
+    orgMatrix <- x$get()
+    inverseOfX <- solve(orgMatrix, ...)
+    x$setCacheSolve(inverseOfX)
+    inverseOfX
 }
